39cd0d14a5cf707f4125d03d09344c457ba0e5e1
Dipesh <109168760+dipeshacademy@users.noreply.github.com>
2024-10-01 21:15:05 +0530

    Update password-generator.c

--- password-generator.c
+++ password-generator.c
@@ -2,28 +2,19 @@
 #include <stdlib.h> 
 #include <time.h>
 
-// Function to generate a random password of length N
+
 void randomPasswordGeneration(int N) 
 {
     int i = 0; 
     int randomizer = 0; 
 
-    // Seed the random-number generator with the current time
-    srand((unsigned int)(time(NULL))); 
-
-    // Character sets for password generation
+    srand((unsigned int)(time(NULL)));
     char numbers[] = "0123456789"; 
     char letter[] = "abcdefghijklmnoqprstuvwyzx"; 
     char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX"; 
     char symbols[] = "!@#$^&*?"; 
-
-    // Array to store the generated password
     char password[N + 1]; 
-
-    // Ensure variety in the first character
     randomizer = rand() % 4; 
-
-    // Generate a password of length N
     for (i = 0; i < N; i++) { 
         if (randomizer == 0) { 
             password[i] = numbers[rand() % 10]; 
@@ -39,20 +30,14 @@ void randomPasswordGeneration(int N)
         } 
         randomizer = rand() % 4; 
     } 
-
-    // Null-terminate the password string
     password[N] = '\0';
-
-    // Print the generated password
     printf("Your password is: %s\n", password); 
 }
 
-// Main function to drive the program
+
 int main() 
 { 
-    int N = 10; // Define the length of the password
-
-    // Call the function to generate the password
+    int N = 10;
     randomPasswordGeneration(N); 
 
     return 0; 
